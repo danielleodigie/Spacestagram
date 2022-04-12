98e805e4f01e803949190c46de00c9a945db4d8c
danielleodigie <odigiedanielle@gmail.com>
2022-04-11 20:23:30 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import './App.css';
- 
+import Particles from "react-tsparticles";
+{/*import LikeBtn from "./LikeBtn";*/}
+
+class LikeBtn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { likes: 0 };
+  }
+
+  inc = () => {
+    this.setState(prvState => ({
+      likes: prvState.likes + 1
+    }));
+  };
+
+  render() {
+    return (
+      <>
+        <button onClick={e => this.inc()} className="click">Kudos {this.state.likes}</button>
+      </>
+    );
+  }
+}
+
 class App extends React.Component {
  
   constructor(props){
@@ -11,6 +34,7 @@ class App extends React.Component {
     };
   }
  
+  
  
   componentDidMount() {
     fetch('https://api.nasa.gov/planetary/apod?api_key=qoXEZ8VFBwU4mhb4XKpFWQ4bMWuLhbWbtNvc2MVb&count=10')
@@ -23,24 +47,24 @@ class App extends React.Component {
     })
   }
 
-  incrementMe = () => {
-    let newCount = this.state.count + 1
-    this.setState({
-      count: newCount
-    })
-
-  }
+  
   render() {
     const { DataisLoaded, items } = this.state;
  
     if (!DataisLoaded) {
-      return <div>Loading...</div>;
+      return (<div><img src="images/loading.gif"></img></div>)
     }
     else {
  
    
     return (
+      
+                
       <div className="App">
+        <header>
+          <h1 id='header'>spacestagram</h1>
+
+        </header>
         <div class="container">
           {items.map(item =>(
             <div class="box">
@@ -48,10 +72,8 @@ class App extends React.Component {
               <h4 class="title">@{item.title}</h4>
               <h6 class="date">{item.date}</h6>
               <p class="info">{item.explanation}</p>
-              <div>
-                <button onClick={this.incrementMe}>❤️</button>
-                Likes: {this.state.count}
-              </div>
+              <div class="button"><LikeBtn/></div>
+              
             </div>
 
           ))} 
